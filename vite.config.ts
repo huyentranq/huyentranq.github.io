c9aaafe565dae7d6f21eb3bf1e3d74f8de0043c0
huyentranq <nguyenhuyentrangg457@gmail.com>
2025-06-17 16:26:49 +0700

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/huyentranq.github.io/', // thay 'your-repo-name' bằng tên repo GitHub của bạn
+  base: '/', // ✅ Đúng cho User Page
   plugins: [react()],
 })
